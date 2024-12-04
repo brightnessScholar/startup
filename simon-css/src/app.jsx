c099655213d63a03925486f3807bbbb056390133
Cadence Potter <cadence@pottershome.com>
2024-12-04 11:39:56 -0700

    git commit -m: "Moved new login function from app.jsx to login.jsx"

--- simon-css/src/app.jsx
+++ simon-css/src/app.jsx
@@ -85,25 +85,6 @@ export default function App() {
       );
 }
 
-export function Login({ userName, authState, onAuthChange }) {
-    return (
-      <main className='container-fluid bg-secondary text-center'>
-        <div>
-          {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
-          {authState === AuthState.Authenticated && <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />}
-          {authState === AuthState.Unauthenticated && (
-            <Unauthenticated
-              userName={userName}
-              onLogin={(loginUserName) => {
-                onAuthChange(loginUserName, AuthState.Authenticated);
-              }}
-            />
-          )}
-        </div>
-      </main>
-    );
-  }
-
 function NotFound() {
     return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
   }
--- simon-css/src/login/login.jsx
+++ simon-css/src/login/login.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
+export function Login({ userName, authState, onAuthChange }) {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState === AuthState.Authenticated && <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+    </main>
+  );
+}
+/*
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
@@ -21,3 +40,5 @@ export function Login() {
     </main>
   );
 }
+
+*/
