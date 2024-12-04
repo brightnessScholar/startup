723f768e23e3a8c8066401623d1a495aa2976b11
Cadence Potter <cadence@pottershome.com>
2024-12-04 10:41:02 -0700

    git commit -m: "Moved main of login.html to login.jsx and imported login.css"

--- simon-css/src/login.jsx
+++ simon-css/src/login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main className="container-fluid bg-secondary text-center">
+      <div>
+        <h1>Welcome to Simon</h1>
+        <form method="get" action="play.html">
+          <div className="input-group mb-3">
+            <span className="input-group-text">@</span>
+            <input className="form-control" type="text" placeholder="your@email.com" />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text">🔒</span>
+            <input className="form-control" type="password" placeholder="password" />
+          </div>
+          <button type="submit" class="btn btn-primary">Login</button>
+          <button type="submit" class="btn btn-secondary">Create</button>
+        </form>
+      </div>
     </main>
   );
 }
